9e6cac979debf64720ade64622ad7e3c1a7302d3
brandong1 <brandon@brandonleeproductions.com>
2020-03-05 14:28:04 -0700

    test new routes

--- helloworld.rb
+++ helloworld.rb
@@ -10,12 +10,6 @@ get '/goodbye' do
   "Bye Mom!!"
 end
 
-get '/about' do
-  output = "<h2>Sinatra</h2>"
-  output << "<p>This is my first Sinatra app.</p>"
-  output
-end
-
 get '/form' do 
   form = '<form action="/form" method="post">'
   form << '<input type="submit" value="Submit" />'
@@ -25,4 +19,18 @@ end
 
 post '/form' do
   "Thank you for submitting the form."
+end
+
+get '/company/about' do
+  output = "<h2>Sinatra</h2>"
+  output << "<p>This is my first Sinatra app.</p>"
+  output
+end
+# Regex
+# get /\A\/company\/[A-Za-z\-]+\Z/ do
+#   "This route matches a Regex."
+# end
+
+get '/company/*' do
+  "This route catches everything else."
 end
